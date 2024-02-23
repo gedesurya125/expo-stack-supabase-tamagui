ccd71bcd1bc3ad417e91e8a381b5b4d2ada1a185
gedesurya125 <gedesurya125@gmail.com>
2024-02-23 18:16:15 +0800

    add xentralFetch function

---
+++ api/xentral/fetchXentral.ts
@@ -0,0 +1,13 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/vnd.xentral.default.v1+json',
+    authorization: `Bearer ${process.env.XENTRAL_PERSONAL_ACCESS_TOKEN}`,
+  },
+};
+
+export const fetchXentral = async (endPoint: string) => {
+  return await fetch(`${process.env.EXPO_PUBLIC_XENTRAL_API_BASE}${endPoint}`, options)
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+};

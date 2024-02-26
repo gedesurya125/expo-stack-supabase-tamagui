2a2a03233a5e23e42a2c7ce442b8c2e03a6acc8b
gedesurya125 <gedesurya125@gmail.com>
2024-02-26 12:06:20 +0800

    Add search functionality to the existing customer

--- api/xentral/getCustomers.ts
+++ api/xentral/getCustomers.ts
@@ -1,8 +1,26 @@
 import { fetchXentral } from './fetchXentral';
 
-export const getCustomers = async ({ pageNumber = 1, pageSize = 20 }) => {
-  const endPoint = `/customers?page[number]=${pageNumber}&page[size]=${pageSize}`;
-  console.log('this is the page number', { endPoint });
+export const getCustomers = async ({
+  pageNumber = 1,
+  pageSize = 20,
+  ...other
+}: {
+  pageNumber: number;
+  pageSize: number;
+  filter?: {
+    key: string;
+    op: 'contains' | 'equals' | 'notEquals';
+    value: string;
+  };
+}) => {
+  let endPoint = `/customers?page[number]=${pageNumber}&page[size]=${pageSize}`;
 
-  return await fetchXentral(`/customers?page[number]=${pageNumber}&page[size]=${pageSize}`);
+  if (other?.filter && other?.filter?.key && other?.filter?.op && other?.filter?.value) {
+    const { key, op, value } = other.filter;
+    endPoint = `${endPoint}&filter[0][key]=${key}&filter[0][op]=${op}&filter[0][value]=${value}`;
+  }
+  console.log('this is the endpoint', endPoint);
+
+  // filter[0][key]=name&filter[0][op]=contains&filter[0][value]=ss
+  return await fetchXentral(endPoint);
 };
--- app/(drawer)/existing-customer.tsx
+++ app/(drawer)/existing-customer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useCallback } from 'react';
 import {
   YStack,
   H2,
@@ -18,49 +18,115 @@ import { XentralCustomer } from '~/types';
 import { FlatList } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
 
-export default function TabTwoScreen() {
-  const [customers, setCustomer] = React.useState<XentralCustomer[]>([]);
+type CustomersContextValue = {
+  customers: XentralCustomer[];
+  nameFilter: string;
+  setNameFilter: Dispatch<SetStateAction<string>>;
+  setCustomers: Dispatch<SetStateAction<XentralCustomer[]>>;
+};
 
-  React.useEffect(() => {
-    const fetchCustomer = async () => {
-      const newCustomer = await getCustomers({ pageNumber: 1, pageSize: 20 });
-      setCustomer(newCustomer?.data);
-    };
+const CustomerContext = React.createContext<CustomersContextValue>({
+  customers: [],
+  nameFilter: '',
+  setNameFilter: () => null,
+  setCustomers: () => null,
+});
 
-    fetchCustomer();
-  }, []);
+const ExistingCustomerContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [customers, setCustomers] = React.useState<XentralCustomer[]>([]);
+  const [nameFilter, setNameFilter] = React.useState('');
 
   return (
-    <Theme>
-      <YStack flex={1} backgroundColor="$background">
-        <FlatList
-          data={customers}
-          renderItem={({ item }) => {
-            return (
-              <ListItem
-                color="$color"
-                backgroundColor="$background"
-                flexDirection="column"
-                alignItems="flex-start">
-                <Text color="$color">{item.general.name}</Text>
-                <Text color="$color">{item.general.email}</Text>
-              </ListItem>
-            );
-          }}
-          keyExtractor={(item) => item.id}
-          ItemSeparatorComponent={Separator}
-          // ? how to make the search bar sticky, source: https://stackoverflow.com/questions/44638286/how-do-you-make-the-listheadercomponent-of-a-react-native-flatlist-sticky
-          ListHeaderComponent={SearchBar}
-          stickyHeaderIndices={[0]}
-          style={{}}
-        />
-      </YStack>
-    </Theme>
+    <CustomerContext.Provider
+      value={{
+        customers,
+        nameFilter,
+        setCustomers,
+        setNameFilter,
+      }}>
+      {children}
+    </CustomerContext.Provider>
+  );
+};
+
+const useCustomerContext = () => React.useContext(CustomerContext);
+// ===========
+
+export default function ExistingCustomer() {
+  return (
+    <ExistingCustomerContextProvider>
+      <Theme>
+        <YStack flex={1} backgroundColor="$background">
+          <CustomerList />
+        </YStack>
+      </Theme>
+    </ExistingCustomerContextProvider>
   );
 }
 
+const CustomerList = () => {
+  const { nameFilter, setCustomers, customers } = useCustomerContext();
+
+  React.useEffect(() => {
+    const fetchCustomer = async () => {
+      const newCustomer = await getCustomers({
+        pageNumber: 1,
+        pageSize: 20,
+        filter: {
+          key: 'name',
+          op: 'contains',
+          value: nameFilter,
+        },
+      });
+      setCustomers(newCustomer?.data);
+    };
+
+    fetchCustomer();
+  }, [nameFilter]);
+
+  // ? callback prevent re render the header
+  const renderHeader = React.useCallback(() => <SearchBar />, []);
+
+  return (
+    <FlatList
+      data={customers}
+      renderItem={({ item }) => {
+        return (
+          <ListItem
+            color="$color"
+            backgroundColor="$background"
+            flexDirection="column"
+            alignItems="flex-start">
+            <Text color="$color">{item.general.name}</Text>
+            <Text color="$color">{item.general.email}</Text>
+          </ListItem>
+        );
+      }}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={Separator}
+      // ? how to make the search bar sticky, source: https://stackoverflow.com/questions/44638286/how-do-you-make-the-listheadercomponent-of-a-react-native-flatlist-sticky
+      ListHeaderComponent={renderHeader}
+      stickyHeaderIndices={[0]}
+      style={{}}
+    />
+  );
+};
+
 const SearchBar = () => {
+  const { setNameFilter } = useCustomerContext();
+
+  const [value, setValue] = React.useState('');
+
   const theme = useTheme();
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setNameFilter(value);
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value]);
 
   return (
     <View paddingTop="$4" backgroundColor="$background" paddingHorizontal="$4" paddingBottom="$2">
@@ -75,7 +141,14 @@ const SearchBar = () => {
             left: getTokenValue('$3', 'space'),
           }}
         />
-        <Input placeholder="search..." paddingLeft="$8" />
+        <Input
+          placeholder="search..."
+          paddingLeft="$8"
+          value={value}
+          onChangeText={(value) => {
+            setValue(value);
+          }}
+        />
       </View>
     </View>
   );

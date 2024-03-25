ee64b2129cd73b83e614ae56336f4d9ec20f8d34
gedesurya125 <gedesurya125@gmail.com>
2024-03-25 16:09:27 +0800

    add KeyboardAvoidingView to new-customer

--- app/(drawer)/new-customer.tsx
+++ app/(drawer)/new-customer.tsx
@@ -1,5 +1,6 @@
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { H2, YStack, Separator, ScrollView } from 'tamagui';
 import { AddCustomerForm, NewCustomerParams } from '~/components/AddCustomerForm';
 
@@ -7,12 +8,18 @@ export default function NewCustomer() {
   const params = useLocalSearchParams<NewCustomerParams>();
 
   return (
-    <YStack backgroundColor="$background" flex={1} padding="$4">
-      <ScrollView>
-        <H2>Add New Customer</H2>
-        <Separator my="$4" />
-        <AddCustomerForm mapboxLocationId={params?.id as string} params={params} />
-      </ScrollView>
-    </YStack>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{
+        flex: 1
+      }}>
+      <YStack backgroundColor="$background" flex={1} padding="$4">
+        <ScrollView>
+          <H2>Add New Customer</H2>
+          <Separator my="$4" />
+          <AddCustomerForm mapboxLocationId={params?.id as string} params={params} />
+        </ScrollView>
+      </YStack>
+    </KeyboardAvoidingView>
   );
 }
